b74b9ae6f39e97b1412a17e8db503a1b7293fca3
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-29 16:40:18 -0700

    working on CSS and HTML for carousel

--- src/about.js
+++ src/about.js
@@ -1,12 +1,1 @@
 let celements = document.querySelectorAll(".celement");
-
-let visibleIndex = 0;
-
-celements.forEach(function(element, elemIndex) {
-    if (elemIndex === visibleIndex){
-        element.style.display = "inline-block";
-    }
-    else {
-        element.style.display = "none";
-    }
-});
