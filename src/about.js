c5fbb99226ea27da3e092eaeb8c29024590fa6c0
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-29 19:59:19 -0700

    doing the wrong approach to carousel: refactoring

--- src/about.js
+++ src/about.js
@@ -5,6 +5,8 @@ function centerCarousel() {
     let winWidth = window.innerWidth;
     let carWidth = myCarousel.childElementCount * 600;
     myCarousel.style.left = "" + ((winWidth-carWidth)/2) + "px";
+    myCarousel.style.width = "" + carWidth + "px";
+    myCarousel.style.overflow = "visible";
 }
 
 window.addEventListener("load", centerCarousel);
