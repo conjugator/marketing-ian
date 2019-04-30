df3832fa52b44bce222d25b1ca6dd1c41a211cd9
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-29 20:27:50 -0700

    fixing carousel element positioning

--- src/about.js
+++ src/about.js
@@ -1,12 +1,20 @@
 let celements = document.querySelectorAll(".celement");
 
 function centerCarousel() {
-    let myCarousel = document.querySelector(".carousel");
+    //let myCarousel = document.querySelector(".carousel");
     let winWidth = window.innerWidth;
-    let carWidth = myCarousel.childElementCount * 600;
-    myCarousel.style.left = "" + ((winWidth-carWidth)/2) + "px";
-    myCarousel.style.width = "" + carWidth + "px";
-    myCarousel.style.overflow = "visible";
+    let carWidth = celements.length * 600;
+    //myCarousel.style.left = "" + ((winWidth-carWidth)/2) + "px";
+    //myCarousel.style.width = "" + carWidth + "px";
+    //myCarousel.style.overflow = "visible";
+    leftPos = (winWidth-carWidth)/2;
+    //for (let n = 0; n < celements.length; n++){
+    //    celements[n].style.left = "" + (leftPos + n*600) + "px";
+    //}
+    //celements[0].style.left = "" + (leftPos) + "px";
+    celements.forEach(function(element){
+        element.style.left = "" + (leftPos) + "px";
+    })
 }
 
 window.addEventListener("load", centerCarousel);
