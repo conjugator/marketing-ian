06ea28bd8312a4bf3a8c6267a1e82687d217fd20
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-29 18:04:20 -0700

    working on JS carousel

--- src/about.js
+++ src/about.js
@@ -1,1 +1,12 @@
 let celements = document.querySelectorAll(".celement");
+
+function centerCarousel() {
+    let myCarousel = document.querySelector(".carousel");
+    let winWidth = window.innerWidth;
+    let carWidth = 1800 //myCarousel.style.width;
+    console.log(carWidth);
+
+    myCarousel.style.leftPos = (winWidth-carWidth)/2;
+}
+
+window.addEventListener("load", centerCarousel);
